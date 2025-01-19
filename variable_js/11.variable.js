92517a8bb98775920cb0d4f349fab0175bcd5025
Carlos Morales <carlosjosemoralesdejesus@gmail.com>
2025-01-19 15:23:18 -0400

    secciones

--- variable_js/11.variable.js
+++ variable_js/11.variable.js
@@ -6,6 +6,10 @@ var miNumero1 = 15;
 console.log(miNumero1);
 
 // let es la forma correcta de definir variable
-
+// los nombre suelen comenzar con letra minuscula 
+// camel case es la notcion que se utiliza en js
 let miNumero2 = 20;
 console.log(miNumero2);
+
+
+
