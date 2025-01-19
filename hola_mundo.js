c381e153717029d61cdf0b8c90f42b448758d7ee
Carlos Morales <carlosjosemoralesdejesus@gmail.com>
2025-01-19 15:18:16 -0400

    variables

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,1 +1,9 @@
 console.log('Hola Mundo de JavaScript');
+
+nombre = 'Juan';
+
+console.log('Hola ' + nombre);
+
+var edad = 32;
+
+console.log('Edad: ' + edad);
---
+++ variable.js
@@ -0,0 +1,11 @@
+
+// el uso de var esta descontinuado
+
+var miNumero1 = 15;
+
+console.log(miNumero1);
+
+// let es la forma correcta de definir variable
+
+let miNumero2 = 20;
+console.log(miNumero2);
