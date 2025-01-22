e56a8a0cb5e84872d8016ea3dcc55b2a34656e13
carlosjosemorales006 <155334948+carlosjosemorales006@users.noreply.github.com>
2025-01-22 19:00:51 +0000

    operadores de comparacion 03

--- 2.operadores/4.operdores_comparacion.js
+++ 2.operadores/4.operdores_comparacion.js
@@ -20,3 +20,29 @@ console.log("a === b", a === b);
 // string interpolation
 console.log(`${a} == ${b} -> ${a == b}`);
 
+// operador distintos
+// compara valor y convierte el tipo de dato si es necesario
+
+console.log(`${a} != ${b} -> ${a != b}`)
+
+// operador distinto exacto
+
+console.log(`${a} !== ${b} -> ${a !== b}`)
+
+// operador menor que <
+
+console.log(`${a} < ${b} -> ${a < b}`)
+
+// menor o igual que 
+
+console.log(`${a} <= ${b} -> ${a <= b}`)
+
+// mayor que
+
+console.log(`${a} > ${b} -> ${a > b }`)
+
+// mayor o igual que 
+
+console.log(`${a} >= ${b} -> ${a >= b }`)
+
+
