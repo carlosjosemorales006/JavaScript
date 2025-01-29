8e40c7823b355a6a4e88eb21d3d76a8c47f36d06
carlosjosemorales006 <155334948+carlosjosemorales006@users.noreply.github.com>
2025-01-29 19:18:27 +0000

    new 20.

--- 2.operadores/precedente_operadores.js
+++ 2.operadores/precedente_operadores.js
@@ -6,9 +6,18 @@
 
 1. Parentesis y Corchetes
 2. Operadores unarios, como -, ++ , --, !
-3. Aritmeticos +
+3. Aritmeticos *, / y %
+4. Aritmeticos +, -
+5. Relacionales <, <=, > y >=
+6. Igualdad == y !=
+7. Logicos && y ||
+8. Asignacion =, =+, etc
 
-
+ej. una expresion se revisa de izquierda a derecha
 
 
 */
+
+
+let a = 12 / 3 + 2 * 3 - 1;
+console.log(a)
---
+++ 3.Condicionales/1.if.js
@@ -0,0 +1,8 @@
+// Sentecia if
+
+let miNumero = 10;
+// revisar si el numero es positivo
+if (miNuemero > 0){
+
+print('Es numero es p')
+}
