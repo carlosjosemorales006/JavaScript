a2eb738556358b963af8f579163e730db888e3cd
carlosjosemorales006 <155334948+carlosjosemorales006@users.noreply.github.com>
2025-02-07 18:23:07 +0000

    arreglos

---
+++ 5.arreglos/1.arreglos.js
@@ -0,0 +1,23 @@
+// Areglos en JS
+// definir un arreglo
+
+let numerosArreglos= [];
+
+// modificar los valores
+
+numerosArreglos[0]= 13;
+numerosArreglos[1]= 20;
+numerosArreglos[4] = 'Hola';
+
+// Leer los valores del arreglo
+
+for(let a=0; a<=4;a++){
+    console.log(`indix #${a} = ${numerosArreglos[a]}`);
+}
+
+let colores =['rojo','azul','verde','blanco'];
+
+for(let color =0; color < colores.length; color ++){
+    console.log(colores[color]);
+}
+
